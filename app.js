91c04428960cbc736bf7be0b9bd624b2a4607e75
Cenk Ozkan <cenk.ozkan@agilatek.com>
2021-04-12 13:25:43 +0300

    fix

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const port = process.env.port || 3000;
-const cors = require('cors');
+const cors = require("cors");
 
 //route
-const API = require('./router/API');
+const API = require("./router/API");
 
 //db
 const db = mongoose.connect(
-  'mongodb://localhost:27017/Api',
+  //process.env.MOGODB_URI || "mongodb://localhost:27017/Api",
+  process.env.MOGODB_URI ||
+    "mongodb+srv://macrografi:2wsx1QAZ<@cluster0.2hafr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
   (err) => {
     if (!err) {
-      console.log('Connected to DB');
+      console.log("Connected to DB");
     }
   }
 );
@@ -27,5 +29,5 @@ app.use(cors());
 app.use(API.router);
 
 app.listen(port, () => {
-  console.log('Gulp is running PORT : ' + port);
+  console.log("Running PORT : " + port);
 });
